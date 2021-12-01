a8008fcaa6d8f0eee81aadfef598e48b7125bf95
Thomas Boutell <tom@apostrophecms.com>
2021-12-01 07:21:04 -0500

    day 1b

--- day-1b.ts
+++ day-1b.ts
@@ -7,18 +7,19 @@ const input:Array<number> = read('day-1.txt', { encoding: 'utf8' })
   .map(line => parseInt(line));
 
 let first:boolean = true;
-let last:number = 0;
 let increments:number = 0;
+let last:number = 0;
 
-for (const depth of input) {
+for (let i:number = 0; (i < (input.length - 2)); i++) {
+  const sum = input[i] + input[i + 1] + input[i + 2];
   if (first) {
     first = false;
   } else {
-    if (last && depth > last) {
+    if (last && sum > last) {
       increments++;
     }
   }
-  last = depth;
+  last = sum;
 }
 
 console.log(increments);
