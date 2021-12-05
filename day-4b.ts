85dc292353a2e031c5e012273a0a19737b5a5f7a
Thomas Boutell <tom@apostrophecms.com>
2021-12-04 19:20:38 -0500

    day 4 solved. Not super elegant, but raised an interesting question about how to construct a generic type without writing its definition again.

---
+++ day-4b.ts
@@ -0,0 +1,99 @@
+import { readFileSync as read } from 'fs';
+
+type Board = Array<Array<number>>;
+
+const input:Array<string> = read('day-4.txt', { encoding: 'utf8' })
+  .split('\n')
+  .map(line => line.trim());
+
+const numbers:Array<number> = splitNumbers(input);
+const boards = splitBoards(input);
+const boardsWon = new Array<boolean>();
+
+console.log(lastWinningScore());
+
+function lastWinningScore() {
+  for (let b = 0; (b < boards.length); b++) {
+    boardsWon[b] = false;
+  }
+
+  for (let i = 0; (i < numbers.length); i++) {
+    let soFar = numbers.slice(0, i);
+    for (let b = 0; (b < boards.length); b++) {
+      if (boardWins(boards[b], soFar)) {
+        boardsWon[b] = true;
+        if (!boardsWon.some(value => value === false)) {
+          return boardScore(boards[b], soFar);
+        }
+      }
+    }
+  }
+}
+
+function boardWins(board:Board, soFar:Array<number>):boolean {
+  for (let y = 0; (y < board.length); y++) {
+    let good = true;
+    for (let x = 0; (x < board[y].length); x++) {
+      if (!soFar.includes(board[y][x])) {
+        good = false;
+      }
+    }
+    if (good) {
+      return true;
+    }
+  }
+  for (let x = 0; (x < board[0].length); x++) {
+    let good = true;
+    for (let y = 0; (y < board.length); y++) {
+      if (!soFar.includes(board[y][x])) {
+        good = false;
+      }
+    }
+    if (good) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function boardScore(board:Board, soFar:Array<number>):number {
+  let sum = 0;
+  for (let y = 0; (y < board.length); y++) {
+    for (let x = 0; (x < board[y].length); x++) {
+      const value = board[y][x];
+      if (!soFar.includes(value)) {
+        sum += value;
+      }
+    }
+  }
+  return sum * soFar[soFar.length - 1];
+}
+
+function splitNumbers(input:Array<string>):Array<number> {
+  return input[0].split(',').map(n => parseInt(n, 10));  
+}
+
+function splitBoards(input:Array<string>):Array<Board> {
+  const boards = new Array<Board>();
+  let board:Array<Array<number>> = createBoard();
+  for (let row = 1; (row < input.length); row++) {
+    if (input[row] === '') {
+      flushBoard();
+      board = createBoard();
+    } else {
+      board.push(input[row].split(/\s+/).map(n => parseInt(n, 10)));
+    }
+  }
+  flushBoard();
+  function createBoard():Board {
+    // How can I just instantiate a Board? "new Board()" throws an error
+    return new Array<Array<number>>();
+  }
+  function flushBoard() {
+    if (board.length) {
+      boards.push(board);
+    }  
+  }
+  return boards;
+}
+
