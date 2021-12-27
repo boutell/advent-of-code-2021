f9908547238dc799ec79a7f046d01a2b7cd12215
Thomas Boutell <tom@apostrophecms.com>
2021-12-27 08:05:16 -0500

    day 17a. typescript helped me remember to parseInt. Otherwise not a terribly typescripty problem. My solution has an obvious fudge factor in it when deciding what range of values to try. I could definitely narrow this down in some ways, but I did have to push it up further than I thought. You can throw the banana quite high and still get it back down in time.

---
+++ day-17a.ts
@@ -0,0 +1,47 @@
+import { readFileSync as read } from 'fs';
+
+let data = read('day-17.txt', { encoding: 'utf8' }).trim();
+const matches = data.match(/target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)/);
+if (!matches) {
+  throw new Error('Invalid input');
+}
+const [ _all, tx1s, tx2s, ty1s, ty2s ] = matches;
+
+const tx1 = parseInt(tx1s);
+const tx2 = parseInt(tx2s);
+const ty1 = parseInt(ty1s);
+const ty2 = parseInt(ty2s);
+
+console.log(tx1, tx2, ty1, ty2);
+
+let max = Math.max(Math.abs(tx1), Math.abs(tx2), Math.abs(ty1), Math.abs(ty2)) * 5;
+
+let highest = -Infinity;
+
+for (let ivy = -max; (ivy < max); ivy++) {
+  for (let ivx = -max; (ivx < max); ivx++) {
+    let x = 0, y = 0;
+    let vx = ivx, vy = ivy;
+    let myHighest = -Infinity;
+    for (let i = 0; (i < max); i++) {
+      x += vx;
+      y += vy;
+      if (y > myHighest) {
+        myHighest = y;
+      }
+      if (vx > 0) {
+        vx--;
+      } else if (vx < 0) {
+        vx++;
+      }
+      vy--;
+      if ((x >= tx1) && (x <= tx2) && (y >= ty1) && (y <= ty2)) {
+        console.log(`${ivx},${ivy} ${myHighest} ${highest}`);
+        highest = Math.max(highest, myHighest);
+        break;
+      }
+    }
+  }
+}
+
+console.log(highest);
